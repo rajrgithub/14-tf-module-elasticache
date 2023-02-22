82046e75a0743ef7f79b6be831f8ec4a0841895a
brajasekharreddy <raj479.mits@gmail.com>
2023-02-22 22:14:27 +0530

    ElastiCache for RedisDB

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
+variable "node_type" {}
+
+
+
