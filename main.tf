7a0c87c4525bf5614133c94af05d0e61b16ae38d
brajasekharreddy <raj479.mits@gmail.com>
2023-02-26 20:57:38 +0530

    single ElastiCache cluster

--- main.tf
+++ main.tf
@@ -34,16 +34,15 @@ resource "aws_security_group" "elasticache" {
   )
 }
 
-resource "aws_elasticache_replication_group" "elasticache" {
-  replication_group_id       = "${var.env}-elasticache"
-  description                = "${var.env}-elasticache"
-  node_type                  = var.node_type
-  port                       = 6379
-  automatic_failover_enabled = true
-  subnet_group_name          = aws_elasticache_subnet_group.default.name
-  security_group_ids         = [aws_security_group.elasticache.id]
-  num_node_groups            = var.num_node_groups
-  replicas_per_node_group    = var.replicas_per_node_group
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id         = "${var.env}-elasticache-cluster"
+  engine             = "redis"
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
+  security_group_ids = [aws_security_group.elasticache.id]
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-elasticache" }
